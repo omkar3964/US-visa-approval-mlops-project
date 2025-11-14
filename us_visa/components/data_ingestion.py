2cd1f3e5567321dbd46d1fc1c522ab718a07c522
omkar3964 <omalbhare@gmail.com>
2025-11-14 12:28:56 +0530

    data validation added.

--- us_visa/components/data_ingestion.py
+++ us_visa/components/data_ingestion.py
@@ -36,8 +36,7 @@ class DataIngestion:
         try:
             logging.info(f"Exporting data from mongodb")
             usvisa_data = USvisaData()
-            dataframe = usvisa_data.export_collection_as_dataframe(collection_name=
-                                                                   self.data_ingestion_config.collection_name)
+            dataframe = usvisa_data.export_collection_as_dataframe(collection_name=self.data_ingestion_config.collection_name)
             logging.info(f"Shape of dataframe: {dataframe.shape}")
             feature_store_file_path  = self.data_ingestion_config.feature_store_file_path
             dir_path = os.path.dirname(feature_store_file_path)
@@ -99,9 +98,7 @@ class DataIngestion:
 
             logging.info("Performed train test split on the dataset")
 
-            logging.info(
-                "Exited initiate_data_ingestion method of Data_Ingestion class"
-            )
+            logging.info("Exited initiate_data_ingestion method of Data_Ingestion class" )
 
             data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
             test_file_path=self.data_ingestion_config.testing_file_path)
